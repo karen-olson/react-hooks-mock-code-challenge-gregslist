e4af58cb17e608c958dfd252f2a2075eeda43266
Karen Olson <karmarie@gmail.com>
2021-09-14 12:03:13 -0500

    Add sort component. Debug search component submit event.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
+/*
+Deliverables: 
+
+State: 
+1) listings: in App
+2) isFavorited: 
+3) search
+
+1) When the app starts, I can see all listings
+      a) useEffect to make a GET request. Where? In the component that renders
+          the listings
+2) I can click the star icon to un/favorite a listing (don't need to persist)
+      a) Create state to track favorited status
+            it is going to change, not passed as a prop, can't calculate it.
+      b) Does each listing need its own state variable? 
+3) I can click the trash can to remove a listing (does need to persist)
+      a) state variable: listings array. Probably in App. 
+4) I can search for listings by their name. 
+      a) No state needed, just add a filter. 
+*/
+
 function App() {
+  const [listings, setListings] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState("none");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((resp) => resp.json())
+      .then((listings) => setListings(listings));
+  }, []);
+
+  function onDeleteListing(id) {
+    const updatedListings = listings.filter(
+      (listing) => listing.id !== parseInt(id)
+    );
+
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then(() => setListings(updatedListings));
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header
+        search={search}
+        setSearch={setSearch}
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+      />
+      <ListingsContainer
+        listings={listings}
+        onDeleteListing={onDeleteListing}
+        search={search}
+        sortBy={sortBy}
+      />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,12 +1,42 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listings, onDeleteListing, search, sortBy }) {
+  // Look for another design pattern for this feature
+
+  let listingsToDisplay;
+
+  const filteredListings = listings.filter((listing) =>
+    listing.description.toLowerCase().includes(search.toLowerCase())
+  );
+
+  function sortListings(a, b) {
+    if (a[sortBy] < b[sortBy]) {
+      return -1;
+    }
+    if (a[sortBy] > b[sortBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const sortedListings = filteredListings.sort(sortListings);
+
+  sortBy !== "none"
+    ? (listingsToDisplay = sortedListings)
+    : (listingsToDisplay = filteredListings);
+
+  const listingCards = listingsToDisplay.map((listing) => (
+    <ListingCard
+      key={listing.id}
+      listing={listing}
+      onDeleteListing={onDeleteListing}
+    />
+  ));
+
   return (
     <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
+      <ul className="cards">{listingCards}</ul>
     </main>
   );
 }
